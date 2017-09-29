a7134c913607a5835ed3b6de9b94f79c138d40fa
Cristiano Couto <crissilv@br.ibm.com>
2017-09-28 23:01:35 -0300

    - Adicionando Diagrama de Classes versão 4
    - Adicionando o Projeto Web (JSF, JPA, Hibernate e PrimeFaces)

---
+++ Projeto/SGPP/src/java/dao/JPAUtil.java
@@ -0,0 +1,17 @@
+package dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JPAUtil {
+    private static EntityManagerFactory emf;
+    public static EntityManager getEntityManager() {
+        if(emf == null)
+            emf = Persistence.createEntityManagerFactory("SgppPU");
+        return emf.createEntityManager();
+    }
+    public void fechaEntityManager(){
+        emf.close();
+    }
+}
